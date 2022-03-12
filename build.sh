2e88b5a19b96435c8e2873ac1ae6e9c3bb577b5f
Avery Hiebert <averyhiebert@gmail.com>
2022-03-12 06:11:55 -0500

    Commit before messing with colors

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-pyinstaller --clean --onefile --add-data images/dejavu_wide16x16_gs_tc.png:images VimRC.py
+# Linux:
+pyinstaller --clean --noconsole --onefile --add-data images/dejavu_wide16x16_gs_tc.png:images VimRC.py
+# Windows:
+#pyinstaller --clean --noconsole --onefile --add-data 'images\dejavu_wide16x16_gs_tc.png;images' VimRC.py
